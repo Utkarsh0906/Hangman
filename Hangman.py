2927375196a8df6cc41dbf352b5b6dba7f998f61
Utkarsh0906 <96988514+Utkarsh0906@users.noreply.github.com>
2023-06-11 19:32:57 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,29 +1,34 @@
 import random
 import stages
 import word_list
+
 word = random.choice(word_list.words)
 life = 5
 list = ["_"]*len(word)
-print("Guess The Word")
+print("Welcome\nGuess The Word")
 print(" ".join(list)) 
+
 while(True):
     n = input("Enter Your Choice: ").lower() 
     c = 0
+    
     for i in range(len(word)):
         if word[i] == n:
             list[i] = n
             c = 1
+    
     if c == 0:
         life -=1   
+    
     print(stages.stages[life])
     print(" ".join(list))
+    
     if(life == 0):
         print(f"Game Over! You loose!!\nThe word was {word}")
         break
+    
     elif("_" not in list):
-        print("You Won!!!")
+        print("Congratulations!!!\nYou Won!!!")
         break
+    
     print(f"You have {life} lives")
-    
-    
-    
